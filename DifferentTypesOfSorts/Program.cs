43901a98781489afa565a7061339b16254fbab56
Koldrif <vorobiov.777@yandex.ru>
2021-06-23 17:26:12 +0300

    Update Program.cs

--- DifferentTypesOfSorts/Program.cs
+++ DifferentTypesOfSorts/Program.cs
@@ -5,19 +5,31 @@ namespace DifferentTypesOfSorts
 {
     class Program
     {
+        readonly static int[] ArrayOfInt = new int[] { 0, 3, 4, 2, 1, 5, 6, 8, 7, 9 };
         static void Main(string[] args)
         {
-            var ArrayOfInt = new int[] { 0, 3, 4, 2, 1, 5, 6, 8, 7, 9 };
-            System.Console.Write("Array before sort:\t");
-            foreach (var item in ArrayOfInt)
+            // Generate an array
+            int[] ArrayOfInt1 = new int[0];
+            var rand = new Random(((int)DateTime.Now.ToOADate()));
+            for (int i = 0; i < 99; i++)
             {
-                System.Console.Write($"[ {item} ] ");
+                Array.Resize(ref ArrayOfInt1, ArrayOfInt1.Length + 1);
+                ArrayOfInt1[i] = rand.Next(999);
             }
-            Sort.bubbleSort(ref ArrayOfInt);
-            System.Console.Write("\nArray after sort:\t");
-            foreach (var item in ArrayOfInt)
+
+
+            //Sort the array
+            System.Console.Write("Массив до сортировки:\t");
+            foreach (var item in ArrayOfInt1)
             {
-                System.Console.Write($"[ {item} ] ");
+                System.Console.Write(" [ {0} ]", item);
+            }
+            System.Console.WriteLine();
+            Sort.bubbleSort(ref ArrayOfInt1);
+            System.Console.Write("Массив после сортровки:\t");
+            foreach (var item in ArrayOfInt1)
+            {
+                System.Console.Write(" [ {0} ]", item);
             }
         }
 
