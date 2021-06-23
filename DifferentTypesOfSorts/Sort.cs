d1c98078c25d512379752675d716e8ba25d6d5f5
Koldrif <vorobiov.777@yandex.ru>
2021-06-23 11:41:07 +0300

    Project returned with righ gitignore

---
+++ DifferentTypesOfSorts/Sort.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace DifferentTypesOfSorts
+{
+    public static class Sort
+    {
+        private static void swap<T>(ref T[] items, int IndLeft, int IndRight)
+        {
+            if (IndLeft != IndRight)
+            {
+                var temp = items[IndLeft];
+                items[IndLeft] = items[IndRight];
+                items[IndRight] = temp;
+            }
+        }
+        public static void bubbleSort<T>(ref T[] inputArr) where T : IComparable
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            // Some code here...
+            stopwatch.Stop();
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                for (int j = i + 1; j < inputArr.Length; j++)
+                {
+                    if (inputArr[i].CompareTo(inputArr[j]) > 0)
+                    {
+                        swap(ref inputArr, i, j);
+                    }
+                }
+            }
+            System.Console.WriteLine();
+            Console.WriteLine($"Код выполнялся: {stopwatch.Elapsed}");
+            return;
+        }
+    }
+}
